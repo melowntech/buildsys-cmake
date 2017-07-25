498f2a0057daa5c2e3b0303edca8b1b0b2e9687d
Vaclav Blazek <vaclav.blazek@citationtech.net>
2017-07-25 19:45:00 +0200

    added more auto stuff

--- include/debian.mk
+++ include/debian.mk
@@ -6,6 +6,11 @@ ifndef PACKAGES
 $(error Missing PACKAGES variable)
 endif
 
+# automatically add PREFIX as CMAKE_INSTALL_PREFIX
+ifdef PREFIX
+CMAKE_FLAGS += -DCMAKE_INSTALL_PREFIX:PATH=$(PREFIX)
+endif
+
 # use this build directory
 DEB_BUILDDIR=obj-$(DEB_BUILD_GNU_TYPE)
 
@@ -20,3 +25,7 @@ override_dh_auto_install:
 		  , DESTDIR=debian/$(DEB_PACKAGE_$(package)) \
 			cmake -DCOMPONENT=$(component) \
 			    -P $(DEB_BUILDDIR)/cmake_install.cmake;))
+
+# override configuration
+override_dh_auto_configure:
+	dh_auto_configure -- $(CMAKE_FLAGS)
