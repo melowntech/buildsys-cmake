36f8f81c120685be26b6e2c31c1114a860bf8913
Vaclav Blazek <vaclav.blazek@citationtech.net>
2018-04-10 14:22:18 +0200

    fixed typo

--- Modules/FindGeographicLib.cmake
+++ Modules/FindGeographicLib.cmake
@@ -2,7 +2,7 @@
 # Once done, this will define
 #
 #  GeographicLib_FOUND - system has GeographicLib
-#  GeographicLib_INCLUDE_DIRS - the GeographicLib include directories
+#  GeographicLib_INCLUDE_DIR - the GeographicLib include directory
 #  GeographicLib_LIBRARIES - link these to use GeographicLib
 
 find_path(GeographicLib_INCLUDE_DIR
@@ -19,5 +19,5 @@ set(GeographicLib_LIBRARIES ${GeographicLib_LIBRARY})
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(GeographicLib DEFAULT_MSG
   GeographicLib_LIBRARIES
-  GeographicLib_INCLUDE_DIRS)
+  GeographicLib_INCLUDE_DIR)
 mark_as_advanced(GeographicLib_INCLUDE_DIR GeographicLib_LIBRARIES)
