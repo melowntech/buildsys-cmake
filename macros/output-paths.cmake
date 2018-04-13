0688bd7af24663f65a6ac2e80dfd4832b77fc355
Tomáš Malý <tomas.maly@melown.com>
2018-04-13 17:01:43 +0200

    file2cpp: overwrite file on windows; cmake outputh paths for libraries

--- macros/output-paths.cmake
+++ macros/output-paths.cmake
@@ -12,17 +12,18 @@ endmacro()
 
 macro(buildsys_library target)
   get_target_property(TYPE ${target} TYPE)
-  if(TYPE STREQUAL "STATIC_LIBRARY")
-    set_target_properties(${target} PROPERTIES ARCHIVE_OUTPUT_DIRECTORY
-      ${CMAKE_BINARY_DIR}/lib)
-  else()
-    set_target_properties(${target} PROPERTIES LIBRARY_OUTPUT_DIRECTORY
-      ${CMAKE_BINARY_DIR}/lib)
-
+  if(NOT TYPE STREQUAL "STATIC_LIBRARY")
     if(BUILDSYS_CUSTOMER_BUILD)
       buildsys_target_post_build_pathstrip(${target})
     endif()
   endif()
+
+  set_target_properties(${target} PROPERTIES ARCHIVE_OUTPUT_DIRECTORY
+    ${CMAKE_BINARY_DIR}/lib)
+  set_target_properties(${target} PROPERTIES LIBRARY_OUTPUT_DIRECTORY
+    ${CMAKE_BINARY_DIR}/lib)
+  set_target_properties(${target} PROPERTIES RUNTIME_OUTPUT_DIRECTORY
+    ${CMAKE_BINARY_DIR}/bin)
 
   set_target_properties(${target} PROPERTIES BUILDSYS_HOME_DIR
     ${CMAKE_CURRENT_LIST_DIR})
