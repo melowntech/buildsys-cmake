cf2896bd3f3f4964b07cc3c17ed5a15bc1fe5662
Vaclav Blazek <vaclav.blazek@melown.com>
2018-05-02 10:57:55 +0200

    win32: fixed find-powershell

--- Modules.Win32/FindPowerShell.cmake
+++ Modules.Win32/FindPowerShell.cmake
@@ -7,19 +7,21 @@
 
 find_program(POWERSHELL_EXECUTABLE
   NAMES powershell
-  DOC "PowerShell command"
+  HINTS "$ENV{SYSTEMROOT}/System32/WindowsPowerShell/v1.0"
+  DOC "PowerShell executable"
 )
 
 INCLUDE(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(POWERSHELL DEFAULT_MSG POWERSHELL_EXECUTABLE)
 
 set(_powershell_command "POWERSHELL_COMMAND-NOTFOUND")
-if(POWERSHELL_FOUND)
+if(POWERSHELL_EXECUTABLE)
   # Calling a script using "-File" doesn't properly return exit codes.
   # Use dot sourcing instead
   # https://connect.microsoft.com/PowerShell/feedback/details/777375/powershell-exe-does-not-set-an-exit-code-when-file-is-used
   set(_powershell_command "${POWERSHELL_EXECUTABLE}" -NoProfile -NonInteractive -executionpolicy bypass .)
 endif()
+
 set(POWERSHELL_COMMAND ${_powershell_command}
   CACHE STRING "Command suitable for running PowerShell scripts."
 )
