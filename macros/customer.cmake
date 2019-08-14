68a5b8b47238d4f52a35576ac4848d5a560b770e
Vaclav Blazek <vaclav.blazek@citationtech.net>
2019-08-14 14:38:17 +0200

    added macro add_customer_sources_with_default

--- macros/customer.cmake
+++ macros/customer.cmake
@@ -18,3 +18,27 @@ macro(add_customer_sources WHAT VAR)
     list(APPEND ${VAR} ${cfile})
   endforeach()
 endmacro()
+
+# add custom sources to VAR based on customer_build and customer values
+macro(add_customer_sources_with_default WHAT VAR)
+  if (BUILDSYS_CUSTOMER_BUILD)
+    message(STATUS "Building ${WHAT} customized for customer "
+      "'${BUILDSYS_CUSTOMER}'.")
+    set(customer ${BUILDSYS_CUSTOMER})
+  elseif(DEFINED BUILDSYS_DEFAULT_CUSTOMER_NAME)
+    message(STATUS "Building ${WHAT} without customization for "
+      "${BUILDSYS_DEFAULT_CUSTOMER_NAME}.")
+    set(customer "${BUILDSYS_DEFAULT_CUSTOMER_NAME}")
+  else()
+    message(STATUS "Building ${WHAT} without any customization.")
+    set(customer "none")
+  endif()
+
+  foreach(file ${ARGN})
+    string(REPLACE "{CUSTOMER}" ${customer} cfile ${file})
+    if(NOT EXISTS ${cfile})
+      string(REPLACE "{CUSTOMER}" none cfile ${file})
+    endif()
+    list(APPEND ${VAR} ${cfile})
+  endforeach()
+endmacro()
