0b008a8eb9ba2ca78aad23ff5e3a97ea2411c743
Vaclav Blazek <vaclav.blazek@citationtech.net>
2020-02-18 11:22:52 +0100

    pathstrip: calling with explicit bash

--- tools/pathstrip/pathstrip.cmake
+++ tools/pathstrip/pathstrip.cmake
@@ -1,19 +1,15 @@
 macro(buildsys_target_post_build_pathstrip target)
   add_custom_command(TARGET ${target}
                      POST_BUILD
-                     COMMAND ${PATHSTRIP_BINARY}
-                     ARGS $<TARGET_FILE:${target}> ${CMAKE_SOURCE_DIR}
+                     COMMAND "/bin/bash"
+                     ARGS ${PATHSTRIP_BINARY}
+                     $<TARGET_FILE:${target}> ${CMAKE_SOURCE_DIR}
                      WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
                      COMMENT "Stripping paths from target ${target}")
 endmacro()
 
 macro(find_pathstrip)
-  get_filename_component(current_dir ${CMAKE_CURRENT_LIST_FILE} PATH)
-
-  find_program(PATHSTRIP_BINARY
-    pathstrip
-    HINTS ${current_dir})
-
+  set(PATHSTRIP_BINARY ${CMAKE_CURRENT_LIST_DIR}/pathstrip)
   message(STATUS "using ${PATHSTRIP_BINARY} as pathstrip")
 endmacro()
 
