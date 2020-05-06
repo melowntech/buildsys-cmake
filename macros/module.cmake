a686a81ade651208c505aa7e647f8d679db4c91c
Vaclav Blazek <vaclav.blazek@citationtech.net>
2020-05-06 23:46:08 +0200

    v1.7: added functionality breakage support + compatibility override

--- macros/module.cmake
+++ macros/module.cmake
@@ -1,5 +1,5 @@
 macro(module_split_version input name compare version)
-  set(rx "^([^>=<]+)([>=<]+)(.*)$")
+  set(rx "^([^>=<~]+)([>=<~]+)(.*)$")
   string(REGEX REPLACE "${rx}" "\\1" ${name} ${input})
   string(REGEX REPLACE "${rx}" "\\2" ${compare} ${input})
   string(REGEX REPLACE "${rx}" "\\3" ${version} ${input})
@@ -37,10 +37,64 @@ function(module_check_version moduleVersion compareOperator checkVersion result)
   endif()
 endfunction()
 
+macro(_module_split_compatibility input name version)
+  set(rx "^([^~]+)~(.*)$")
+  string(REGEX REPLACE "${rx}" "\\1" ${name} "${input}")
+  string(REGEX REPLACE "${rx}" "\\2" ${version} "${input}")
+  if (${version} STREQUAL ${name})
+    set(${version} "")
+  endif()
+endmacro()
+
 file(WRITE ${CMAKE_BINARY_DIR}/module.list)
 
 macro(_update_module_list TYPE MODULE)
   file(APPEND ${CMAKE_BINARY_DIR}/module.list "${TYPE}: ${MODULE}\n")
+endmacro()
+
+macro(_module_split_breaks input version what)
+  set(rx "^([^:]+)(:)\"?([^\"]*)\"?$")
+  string(REGEX REPLACE "${rx}" "\\1" ${version} ${input})
+  string(REGEX REPLACE "${rx}" "\\2" colon ${input})
+  string(REGEX REPLACE "${rx}" "\\3" ${what} ${input})
+endmacro()
+
+macro(_collect_broken broken atom version cversion)
+  set(BREAKS "${MODULE_${atom}_BREAKS}")
+
+  list(LENGTH BREAKS clen)
+  if ("${version}" AND clen GREATER 1)
+    math(EXPR clen "${clen} - 1")
+    foreach(ci RANGE 0 ${clen} 2)
+      list(GET BREAKS ${ci} bversion)
+      if ("${cversion}" VERSION_GREATER_EQUAL bversion)
+        # ignore
+      elseif (${version} VERSION_LESS bversion)
+        math(EXPR ci "${ci} + 1")
+        list(GET BREAKS ${ci} bwhat)
+        list(APPEND ${broken} "${atom}" "${bversion}" "${bwhat}")
+      endif()
+    endforeach()
+  endif()
+endmacro()
+
+macro(_resolve_broken msg broken)
+  list(LENGTH broken blen)
+  if(blen GREATER 2)
+    math(EXPR blen "${blen} - 1")
+    foreach(bi RANGE 0 ${blen} 3)
+      list(GET broken ${bi} bname)
+      math(EXPR bi "${bi} + 1")
+      list(GET broken ${bi} bversion)
+      math(EXPR bi "${bi} + 1")
+      list(GET broken ${bi} bwhat)
+
+      set(${msg} "${${msg}}\n  ${bname}=${MODULE_${bname}_VERSION} deviates from \
+\"${bwhat}\" in versions before ${bversion}.\n  Either mark ${bname} dependency \
+as compatible by appending ~${bversion}\n  or update your code and ONLY then \
+update dependency to ${bname}>=${bversion}.")
+    endforeach()
+  endif()
 endmacro()
 
 # Define module (library or binary)
@@ -67,19 +121,18 @@ macro(define_module MODULE_TYPE MODULE_NAME_VERSION DEPENDS)
   unset(libs)
   unset(missing)
   unset(definitions)
+  unset(module_breaks)
+  unset(broken)
 
-  set(depends_mode TRUE)
+  set(supported_modes "DEPENDS;DEFINITIONS;BREAKS")
+  set(mode "DEPENDS")
 
   foreach(atom ${ARGN})
-    if(${atom} STREQUAL "DEFINITIONS")
-      # switch to depends definitions
-      set(depends_mode FALSE)
+    if(atom IN_LIST supported_modes)
+      # switch to new mode
+      set(mode "${atom}")
 
-    elseif(${atom} STREQUAL "DEPENDS")
-      # switch to depends mode
-      set(depends_mode TRUE)
-
-    elseif(depends_mode)
+    elseif(mode STREQUAL "DEPENDS")
       set(fullAtom "${atom}")
       module_split_version(${atom} atom compareOperator version)
 
@@ -92,8 +145,11 @@ macro(define_module MODULE_TYPE MODULE_NAME_VERSION DEPENDS)
             LIST(APPEND missing ${atom})
           endif()
         else()
+          _module_split_compatibility("${version}" version cversion)
           module_check_version("${MODULE_${atom}_VERSION}" "${compareOperator}"
             "${version}" checkResult)
+
+          _collect_broken(broken "${atom}" "${version}" "${cversion}")
 
           if(NOT checkResult)
             LIST(APPEND missing ${fullAtom})
@@ -121,10 +177,18 @@ macro(define_module MODULE_TYPE MODULE_NAME_VERSION DEPENDS)
           endif()
       endif()
 
-    else()
+    elseif(mode STREQUAL "DEFINITIONS")
       # definition
       list(APPEND definitions ${atom})
 
+    elseif(mode STREQUAL "BREAKS")
+      # breaks version
+      _module_split_breaks("${atom}" breaks_version breaks_what)
+      list(APPEND module_breaks "${breaks_version}")
+      list(APPEND module_breaks "${breaks_what}")
+      
+    else()
+      message(FATAL_ERROR "${MODULE_NAME}: INTERNAL ERROR unsupported mode ${mode}")
     endif()
   endforeach()
 
@@ -133,7 +197,12 @@ macro(define_module MODULE_TYPE MODULE_NAME_VERSION DEPENDS)
     message(FATAL_ERROR "${MODULE_NAME}: missing dependencies: ${missing}")
   endif()
 
+  _resolve_broken(msg "${broken}")
 
+  if(msg)
+    message(FATAL_ERROR "${MODULE_NAME}: broken functionality:${msg}")
+  endif()
+  
   # extra libraries that has to be linked to binaries (injected dependencies
   # from outside)
   if(TYPE_BINARY)
@@ -159,6 +228,12 @@ macro(define_module MODULE_TYPE MODULE_NAME_VERSION DEPENDS)
   if(module_version)
     set(MODULE_${MODULE_NAME}_VERSION ${module_version})
     set(MODULE_${MODULE_NAME}_VERSION ${module_version} PARENT_SCOPE)
+  endif()
+
+  # set BREAKS
+  if(module_breaks)
+    set(MODULE_${MODULE_NAME}_BREAKS ${module_breaks})
+    set(MODULE_${MODULE_NAME}_BREAKS ${module_breaks} PARENT_SCOPE)
   endif()
 
   # mark as found
