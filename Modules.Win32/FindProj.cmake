57ff6e7fd459beea15be98d1d5fe6c254453c8a7
Vaclav Blazek <vaclav.blazek@gmail.com>
2020-07-17 13:07:29 +0200

    FindProj: added PROJ_VERSION

--- Modules.Win32/FindProj.cmake
+++ Modules.Win32/FindProj.cmake
@@ -4,6 +4,7 @@
 #  PROJ_FOUND - system has Proj library
 #  PROJ_INCLUDE_DIR - the Proj include directories
 #  PROJ_LIBRARIES - link these to use Proj library
+#  PROJ_VERSION - Proj library version
 
 # find PROJ4 installation
 find_package(PROJ4 REQUIRED)
@@ -11,3 +12,5 @@ find_package(PROJ4 REQUIRED)
 set(PROJ_INCLUDE_DIR "${PROJ4_INCLUDE_DIR}")
 set(PROJ_LIBRARIES proj)
 set(PROJ_FOUND TRUE)
+
+# TODO: set PROJ_VERSION
