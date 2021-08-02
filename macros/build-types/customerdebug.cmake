7d663e3f35744f0d8997debdbef97f7bb3ffa5e6
Vaclav Blazek <vaclav.blazek@citationtech.net>
2021-08-02 12:14:40 +0200

    customer support: dropped CMP0043 OLD behaviour dependency

--- macros/build-types/customerdebug.cmake
+++ macros/build-types/customerdebug.cmake
@@ -5,9 +5,19 @@ set(CMAKE_CXX_FLAGS_CUSTOMERDEBUG "${CMAKE_CXX_FLAGS_DEBUG}")
 set(CMAKE_C_FLAGS_CUSTOMERDEBUG "${CMAKE_C_FLAGS_DEBUG}")
 set(CMAKE_EXE_LINKER_FLAGS_CUSTOMERDEBUG "${CMAKE_EXE_LINKER_FLAGS_DEBUG}")
 
-# clone definitions from Debug and add custom definitions
-get_property(defs DIRECTORY PROPERTY COMPILE_DEFINITIONS_DEBUG)
-set_property(DIRECTORY PROPERTY COMPILE_DEFINITIONS_CUSTOMERDEBUG ${defs}
-  BUILDSYS_CUSTOMER_BUILD=1)
+cmake_policy(GET CMP0043 CMP0043)
+if(CMP0043 STREQUAL OLD)
+  # Old way:
+  # clone definitions from Debug and add custom definitions
+  get_property(defs DIRECTORY PROPERTY COMPILE_DEFINITIONS_DEBUG)
+  set_property(DIRECTORY PROPERTY COMPILE_DEFINITIONS_CUSTOMERDEBUG ${defs}
+    BUILDSYS_CUSTOMER_BUILD=1)
+else()
+  # use generator
+  # there are probably no compile definitions for Release (yet)
+  set_property(DIRECTORY APPEND PROPERTY
+    COMPILE_DEFINITIONS $<$<CONFIG:CustomerDebug>:BUILDSYS_CUSTOMER_BUILD=1>
+    )
+endif()
 
 set(BUILDSYS_CUSTOMER_BUILD_CUSTOMERDEBUG TRUE)
