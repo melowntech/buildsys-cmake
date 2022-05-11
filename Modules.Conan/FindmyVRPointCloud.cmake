cb9805a3dc24dc88be69aa7bcecba632f320762d
Tomáš Pazdiora <tomas.pazdiora@melowntech.com>
2022-05-11 18:45:34 +0200

    bugfix

--- Modules.Conan/FindmyVRPointCloud.cmake
+++ Modules.Conan/FindmyVRPointCloud.cmake
@@ -1,3 +1,10 @@
+# --------- backup variables ---------
+set(_jsoncpp_FOUND ${jsoncpp_FOUND})
+set(_jsoncpp_VERSION ${jsoncpp_VERSION})
+set(_jsoncpp_LIBRARIES ${jsoncpp_LIBRARIES})
+set(_jsoncpp_INCLUDE_DIRS ${jsoncpp_INCLUDE_DIRS})
+# ------------------------------------
+
 find_package(myVRPointCloud CONFIG)
 
 if (TARGET myVRPointCloud::myVRPointCloud)
@@ -6,3 +13,10 @@ if (TARGET myVRPointCloud::myVRPointCloud)
   set(myVRPointCloud_LIBRARIES myVRPointCloud::myVRPointCloud)
   add_library(myVRPointCloud ALIAS myVRPointCloud::myVRPointCloud)
 endif()
+
+# --------- restore variables ---------
+set(jsoncpp_FOUND ${_jsoncpp_FOUND})
+set(jsoncpp_VERSION ${_jsoncpp_VERSION})
+set(jsoncpp_LIBRARIES ${_jsoncpp_LIBRARIES})
+set(jsoncpp_INCLUDE_DIRS ${_jsoncpp_INCLUDE_DIRS})
+# ------------------------------------
