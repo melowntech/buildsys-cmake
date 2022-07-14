242ebe291e27edf0f1fdd4268dd0a06b36cff0e4
Mark Karpilovskij <mark.karpilovskij@melowntech.com>
2022-07-14 14:47:29 +0200

    Add tinyxml2::tinyxml2 target for P3DR compatibility

--- Modules/Findtinyxml2.cmake
+++ Modules/Findtinyxml2.cmake
@@ -28,3 +28,14 @@ find_package_handle_standard_args(tinyxml2 DEFAULT_MSG
   TINYXML2_LIBRARIES
   TINYXML2_INCLUDE_DIRS)
 mark_as_advanced(TINYXML2_INCLUDE_DIR TINYXML2_LIBRARIES)
+
+# Add tinyxml2::tinyxml2 target for P3DR compatibility
+if(TINYXML2_FOUND AND NOT TARGET tinyxml2::tinyxml2)
+  add_library(tinyxml2::tinyxml2 UNKNOWN IMPORTED)
+  set_target_properties(tinyxml2::tinyxml2 PROPERTIES
+    IMPORTED_LINK_INTERFACE_LANGUAGES "CXX"
+    IMPORTED_LOCATION "${TINYXML2_LIBRARIES}"
+    INTERFACE_INCLUDE_DIRECTORIES
+      "${TINYXML2_INCLUDE_DIR}"
+    )
+endif()
