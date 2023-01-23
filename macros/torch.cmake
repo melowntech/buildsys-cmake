d499fe7a6495191d4b22d934b93e6bddf032874b
Tomáš Pazdiora <tomas.pazdiora@melowntech.com>
2023-01-23 19:33:17 +0000

    BUILDSYS_CONAN torch without python

--- macros/torch.cmake
+++ macros/torch.cmake
@@ -1,14 +1,18 @@
 macro(enable_torch_impl)
-  if(NOT PYTHON_EXECUTABLE)
-    message(STATUS "No PYTHON_EXECUTABLE found. Use enable_python(version).")
+  if(NOT BUILDSYS_CONAN)
+    if(NOT PYTHON_EXECUTABLE)
+      message(STATUS "No PYTHON_EXECUTABLE found. Use enable_python(version).")
+    else()
+      execute_process(COMMAND "${PYTHON_EXECUTABLE}" -c
+        "import torch.utils; print(torch.utils.cmake_prefix_path)\n"
+        OUTPUT_VARIABLE TORCH_INSTALL_PREFIX
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+        ERROR_QUIET)
+    endif()
+    find_package(Torch REQUIRED PATHS ${TORCH_INSTALL_PREFIX} NO_DEFAULT_PATH)
   else()
-    execute_process(COMMAND "${PYTHON_EXECUTABLE}" -c
-      "import torch.utils; print(torch.utils.cmake_prefix_path)\n"
-      OUTPUT_VARIABLE TORCH_INSTALL_PREFIX
-      OUTPUT_STRIP_TRAILING_WHITESPACE
-      ERROR_QUIET)
+    find_package(Torch REQUIRED)
   endif()
-  find_package(Torch REQUIRED PATHS ${TORCH_INSTALL_PREFIX} NO_DEFAULT_PATH)
   
   if (MSVC)
     file(GLOB TORCH_DLLS "${TORCH_INSTALL_PREFIX}/lib/*.dll")
