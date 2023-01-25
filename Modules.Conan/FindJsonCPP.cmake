eafdb0c6cfc54ba3bc1d561285647e21a01c1af1
Tomáš Pazdiora <tomas.pazdiora@melowntech.com>
2023-01-25 11:01:30 +0000

    update

--- Modules.Conan/FindJsonCPP.cmake
+++ Modules.Conan/FindJsonCPP.cmake
@@ -1,10 +1,3 @@
-# --------- backup variables ---------
-set(_jsoncpp_FOUND ${jsoncpp_FOUND})
-set(_jsoncpp_VERSION ${jsoncpp_VERSION})
-set(_jsoncpp_LIBRARIES ${jsoncpp_LIBRARIES})
-set(_jsoncpp_INCLUDE_DIRS ${jsoncpp_INCLUDE_DIRS})
-# ------------------------------------
-
 find_package(jsoncpp CONFIG)
 
 if (TARGET jsoncpp::jsoncpp)
@@ -12,10 +5,3 @@ if (TARGET jsoncpp::jsoncpp)
   set(JSONCPP_VERSION ${jsoncpp_VERSION})
   set(JSONCPP_LIBRARIES jsoncpp::jsoncpp)
 endif()
-
-# --------- restore variables ---------
-set(jsoncpp_FOUND ${_jsoncpp_FOUND})
-set(jsoncpp_VERSION ${_jsoncpp_VERSION})
-set(jsoncpp_LIBRARIES ${_jsoncpp_LIBRARIES})
-set(jsoncpp_INCLUDE_DIRS ${_jsoncpp_INCLUDE_DIRS})
-# ------------------------------------
