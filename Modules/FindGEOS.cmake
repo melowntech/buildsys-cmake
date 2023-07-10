092214f9a7489e2d2fbea95c124fceef83d8383f
Tomáš Pazdiora <tomas.pazdiora@melowntech.com>
2023-07-10 09:58:14 +0000

    add find for geos

--- Modules/FindGEOS.cmake
+++ Modules/FindGEOS.cmake
@@ -1,34 +1,7 @@
-# - Try to find GEOS
-# Once done, this will define
-#
-#  GEOS_FOUND - system has GEOS
-#  GEOS_INCLUDE_DIRS - the GEOS include directories
-#  GEOS_LIBRARIES - link these to use GEOS
+find_package(GEOS CONFIG)
 
-# Use pkg-config to get hints about paths
-find_package(PkgConfig)
-pkg_check_modules(GEOS_PKGCONF QUIET geos)
-
-find_path(GEOS_INCLUDE_DIR
-  NAMES geos_c.h
-  PATHS ${GEOS_PKGCONF_INCLUDE_DIRS}
-)
-
-# Finally the library itself
-find_library(GEOS_LIBRARY
-  NAMES geos_c
-  PATHS ${GEOS_PKGCONF_LIBRARY_DIRS}
-)
-
-set(GEOS_INCLUDE_DIRS ${GEOS_INCLUDE_DIR})
-set(GEOS_LIBRARIES ${GEOS_LIBRARY})
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GEOS
-  FOUND_VAR GEOS_FOUND
-  REQUIRED_VARS
-  GEOS_LIBRARIES
-  GEOS_INCLUDE_DIRS
-  )
-
-mark_as_advanced(GEOS_INCLUDE_DIR GEOS_LIBRARIES)
+if (TARGET GEOS::geos)
+  set(GEOS_FOUND TRUE)
+  set(GEOS_VERSION ${GEOS_VERSION})
+  set(GEOS_LIBRARIES GEOS::geos GEOS::geos_cxx_flags)
+endif()
